d35abbd0b499472e041f8b3dca3b1c659531fdc1
Baraghin Artiom <artbaraghin_05@mail.ru>
2026-04-27 23:48:55 +0300

    feat: add DataAnnotations validation to DTOs

--- backend/LearnFlow.Domain/Models/User/UserRegisterDto.cs
+++ backend/LearnFlow.Domain/Models/User/UserRegisterDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LearnFlow.Domain.Models.User
 {
     public class UserRegisterDto
     {
+        [Required(ErrorMessage = "Numele este obligatoriu.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Numele trebuie sa aiba intre 2 si 100 caractere.")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email-ul este obligatoriu.")]
+        [EmailAddress(ErrorMessage = "Format email invalid.")]
+        [StringLength(150)]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Parola este obligatorie.")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Parola trebuie sa aiba minim 4 caractere.")]
         public string Password { get; set; } = string.Empty;
     }
 }
