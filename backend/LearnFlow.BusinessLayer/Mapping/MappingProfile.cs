0e6178ab4c54755387aa5df95bea920cf64a7f5f
Cotelea Andrei <260632733+andreicotelea@users.noreply.github.com>
2026-04-27 23:21:41 +0300

    feat: add MappingProfile for Lesson and Category

--- backend/LearnFlow.BusinessLayer/Mapping/MappingProfile.cs
+++ backend/LearnFlow.BusinessLayer/Mapping/MappingProfile.cs
@@ -1,6 +1,10 @@
 using AutoMapper;
 using LearnFlow.Domain.Entities.User;
+using LearnFlow.Domain.Entities.Lesson;
+using LearnFlow.Domain.Entities.Category;
 using LearnFlow.Domain.Models.User;
+using LearnFlow.Domain.Models.Lesson;
+using LearnFlow.Domain.Models.Category;
 
 namespace LearnFlow.BusinessLayer.Mapping
 {
@@ -14,6 +18,21 @@ namespace LearnFlow.BusinessLayer.Mapping
 
             CreateMap<UserRegisterDto, UserData>()
                 .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+
+            // Lesson mappings
+            CreateMap<LessonData, LessonDto>()
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category != null ? src.Category.Name : ""))
+                .ForMember(dest => dest.Students, opt => opt.MapFrom(src => src.StudentCount))
+                .ForMember(dest => dest.Profesor, opt => opt.MapFrom(src => src.ProfesorName))
+                .ForMember(dest => dest.Locked, opt => opt.MapFrom(src => src.IsLocked));
+
+            CreateMap<CreateLessonDto, LessonData>();
+
+            // Category mappings
+            CreateMap<CategoryData, CategoryDto>()
+                .ForMember(dest => dest.LessonCount, opt => opt.MapFrom(src => src.Lessons.Count));
+
+            CreateMap<CreateCategoryDto, CategoryData>();
         }
     }
 }
