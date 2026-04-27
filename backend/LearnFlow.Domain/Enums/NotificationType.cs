09662b328a328ba06263fbc2bb46bd14bbaedfc0
Baraghin Artiom <artbaraghin_05@mail.ru>
2026-04-28 01:09:41 +0300

    feat: add NotificationType enum to Domain/Enums

---
+++ backend/LearnFlow.Domain/Enums/NotificationType.cs
@@ -0,0 +1,11 @@
+namespace LearnFlow.Domain.Enums
+{
+    public enum NotificationType
+    {
+        Info = 0,
+        Success = 1,
+        Warning = 2,
+        Error = 3,
+        Achievement = 4
+    }
+}
