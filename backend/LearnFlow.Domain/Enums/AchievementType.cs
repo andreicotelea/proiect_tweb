2630bc70226144acb544f6403ea9db84e2961713
Baraghin Artiom <artbaraghin_05@mail.ru>
2026-04-28 01:10:11 +0300

    feat: add AchievementType enum to Domain/Enums

---
+++ backend/LearnFlow.Domain/Enums/AchievementType.cs
@@ -0,0 +1,11 @@
+namespace LearnFlow.Domain.Enums
+{
+    public enum AchievementType
+    {
+        LessonComplete = 0,
+        StreakDays = 1,
+        TopLeaderboard = 2,
+        CategoryMaster = 3,
+        PerfectScore = 4
+    }
+}
